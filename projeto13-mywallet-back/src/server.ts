69cfe763fe6a188e5a7526d2b713bf86c730d760
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-06-30 08:50:36 -0300

    enviroment variables

--- projeto13-mywallet-back/src/server.ts
+++ projeto13-mywallet-back/src/server.ts
@@ -1,7 +1,9 @@
+import { config } from "dotenv";
 import express from "express";
 
+config();
 const app = express();
 
-app.listen(5000, () =>
+app.listen(process.env.SERVER_PORT, () =>
   console.log("Server is running at http://localhost:5000")
 );
